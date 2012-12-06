4c5fb5584bbd70c0214c8d81b85da1b4aaad2466
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-12-06 14:18:43 +0100

    fixed cov bug, added auth

---
+++ ipython_notebook_config.py
@@ -0,0 +1,13 @@
+import os
+c = get_config()
+
+# Kernel config
+c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
+
+# Notebook config
+c.NotebookApp.certfile = os.path.join(os.getcwd(), u'mycert.pem')
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+# It's a good idea to put it on a known, fixed port
+c.NotebookApp.port = 9999
+c.NotebookApp.password = u'sha1:60e7d2645fb4:97064d28bad2a4a12950055c830d9637b652c9ec'
