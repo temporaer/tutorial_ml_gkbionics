da1f41dd5b850e4c70ba336e8722093bbdd39f26
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-12-12 20:03:02 +0100

    Starting on logistic regression practial stuff

---
+++ utility.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_decision_boundary(clf, X):
+    w = clf.coef_.ravel()
+    a = -w[0] / w[1]
+    xx = np.linspace(np.min(X[:, 0]), np.max(X[:, 0]))
+    yy = a * xx - clf.intercept_ / w[1]
+    plt.plot(xx, yy)
