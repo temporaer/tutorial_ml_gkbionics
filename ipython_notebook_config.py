0bd0673bb00dee8c0ccfcee1f7e7c8ef35c47740
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-12-13 18:24:50 +0100

    simplified config

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -5,9 +5,9 @@ c = get_config()
 c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
 
 # Notebook config
-c.NotebookApp.certfile = os.path.join(os.getcwd(), u'mycert.pem')
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
+#c.NotebookApp.certfile = os.path.join(os.getcwd(), u'mycert.pem')
+#c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = True
 # It's a good idea to put it on a known, fixed port
 c.NotebookApp.port = 9999
-c.NotebookApp.password = u'sha1:60e7d2645fb4:97064d28bad2a4a12950055c830d9637b652c9ec'
+#c.NotebookApp.password = u'sha1:60e7d2645fb4:97064d28bad2a4a12950055c830d9637b652c9ec'
