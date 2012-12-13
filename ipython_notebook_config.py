ce3364c22704ae96f1849284110e8f87076647b3
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-12-13 19:24:18 +0100

    changing workdir

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -8,7 +8,7 @@ c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
 #c.NotebookApp.certfile = os.path.join(os.getcwd(), u'mycert.pem')
 #c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = True
-c.NotebookApp.notebook_dir = os.path.join(os.env("HOME"), "Desktop", "Tutorial")
+c.NotebookManager.notebook_dir = os.path.join(os.env("HOME"), "Desktop", "Tutorial")
 # It's a good idea to put it on a known, fixed port
 c.NotebookApp.port = 9999
 #c.NotebookApp.password = u'sha1:60e7d2645fb4:97064d28bad2a4a12950055c830d9637b652c9ec'
