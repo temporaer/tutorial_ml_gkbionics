efe6c0391828fb35fc9d6dc8e96822def43aa4d1
Hannes Schulz <schulz@ais.uni-bonn.de>
2012-12-13 18:22:08 +0100

    utility: added plotEllipse

--- utility.py
+++ utility.py
@@ -10,3 +10,16 @@ def plot_decision_boundary(clf, X):
     plt.plot(xx, yy)
     plt.xticks(())
     plt.yticks(())
+
+def plotEllipse(pos,P,edge='k',face='none',line_width='.1'):
+    from numpy.linalg import svd
+    from matplotlib.patches import Ellipse
+    import math
+    from numpy import pi
+    from matplotlib.pyplot import gca
+    U, s , Vh = svd(P)
+    orient = math.atan2(U[1,0],U[0,0])*180/pi
+    ellipsePlot = Ellipse(xy=pos, width=2.0*math.sqrt(s[0]), height=2.0*math.sqrt(s[1]), angle=orient,facecolor=face, edgecolor=edge, lw=line_width)
+    ax = gca()
+    ax.add_patch(ellipsePlot);
+    return ellipsePlot
